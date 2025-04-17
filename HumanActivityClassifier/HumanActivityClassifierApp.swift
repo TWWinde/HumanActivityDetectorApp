b8c8f00cfdf5cd6cd4273ba08c7794bf23fce2b2
TWWinde <st180408@stud.uni-stuttgart.de>
2025-04-17 19:26:56 +0200

    Initial Commit

---
+++ HumanActivityClassifier/HumanActivityClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumanActivityClassifierApp.swift
+//  HumanActivityClassifier
+//
+//  Created by Tang Wenwu on 17.04.25.
+//
+
+import SwiftUI
+
+@main
+struct HumanActivityClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
